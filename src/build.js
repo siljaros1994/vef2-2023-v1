26fc86ba0c62fc11eeebd180de5cb0a458ec49c5
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-02-02 23:24:45 +0000

    Update build.js

--- src/build.js
+++ src/build.js
@@ -1,81 +1,78 @@
 import { join } from 'path';
-import { mkdir, writeFile } from 'fs/promises';
-import { indexTemplate, statsTemplate } from '../lib/html.js';
-import { direxists, readFile } from '../lib/file.js';
+import { mkdir, writeFile } from "fs/promises";
+import { indexTemplate, statsTemplate } from "../lib/html.js";
+import { direxists, readFile } from "../lib/file.js";
 //import { parse } from '../lib/parser.js';
-import { parseCSV } from '../lib/parser.js';
+import { parseCSV } from "../lib/parser.js";
 
-const folderPath = 'C:/Users/silja/Documents/GitHub/vef2-2023-v1/data';
+const folderPath = "./data";
+
 
 const csvFiles = [
-  'hagfraedi.csv',
-  'islenska.csv',
-  'ivt.csv',
-  'laeknadeild.csv',
-  'ologlegt.csv',
-  'salfraedideild.csv',
-  'stjornmalafraedi.csv',
-  'tomt.csv',
-  'vidskiptafraedi.csv',
+    "hagfraedi.csv",
+    "islenska.csv",
+    "ivt.csv",
+    "laeknadeild.csv",
+    "ologlegt.csv",
+    "salfraedideild.csv",
+    "stjornmalafraedi.csv",
+    "tomt.csv",
+    "vidskiptafraedi.csv",
 ];
 
 csvFiles.forEach((file) => {
-  const filePath = `${folderPath}/${file}`;
-  parseCSV(filePath);
+    const filePath = `${folderPath}/${file}`;
+    parseCSV(filePath);
 });
 
-const DATA_DIR = './data';
-const OUTPUT_DIR = './dist';
+const DATA_DIR = "./data";
+const OUTPUT_DIR = "./dist";
 
 async function main() {
-  if (!(await direxists(OUTPUT_DIR))) {
-    await mkdir(OUTPUT_DIR);
-  }
-
-  const jsonData = await readFile('./data/index.json', 'utf-8');
-  const parse = JSON.parse(jsonData);
-
-  const results = [];
-
-  for (const file of parse) {
-    const title = Object.values(file)[1];
-    const description = Object.values(file)[0];
-    const csv = Object.values(file)[3];
-
-    if (file) {
-      const filename = `${title}.html`;
-      const content = await readFile(join(DATA_DIR, title), {
-        encoding: 'utf-8',
-      });
-      const afangar = await JSON.parse(content);
-
-      if (afangar) {
-        const result = {
-          title,
-          afangar,
-          description,
-          csv,
-        };
-
-        results.push(result);
-        const filepath = join(OUTPUT_DIR, filename, 'index.html');
-        console.log(`Writing HTML to: ${filepath}`);
-        const template = indexTemplate(results);
-        await writeFile(filepath, template, { flag: 'w+' });
-      }
+    if (!(await direxists(OUTPUT_DIR))) {
+        await mkdir(OUTPUT_DIR);
     }
-  }
 
-  const filepath = join(OUTPUT_DIR, 'index.html');
-  console.log(`Writing HTML to: ${filepath}`);
-  const template = indexTemplate(results);
-  await writeFile(
-    path.join(__dirname, '..', 'public', 'index.html'),
-    template,
-    (err) => {
-      if (err) throw err;
-      console.log('index.html written');
+    const jsonData = await readFile("./data/index.json", "utf-8");
+    const parse = JSON.parse(jsonData);
+
+    const results = [];
+
+    for (const file of parse) {
+        const title = Object.values(file)[1];
+        const description = Object.values(file)[0];
+        const csv = Object.values(file)[3];
+
+        if (file) {
+            const filename = `${title}.html`;
+            const content = await readFile(join(DATA_DIR, title), "utf-8");
+            const afangar = await JSON.parse(content);
+
+            if (afangar) {
+                const result = {
+                    title,
+                    afangar,
+                    description,
+                    csv,
+                };
+
+                results.push(result);
+                const filepath = join(OUTPUT_DIR, filename, "index.html");
+                console.log(`Writing HTML to: ${filepath}`);
+                const template = indexTemplate(results);
+                await writeFile(filepath, template, { flag: "w+" });
+
+                const statsTitle = 'Statistics';
+                const statsResult = { file: `./${title}.csv` };
+                const statsHtml = await statsTemplate(statsTitle, statsResult);
+                console.log(statsHtml);
+            }
+        }
     }
-  );
-  main().catch(console.error);
+    const filepath = join(OUTPUT_DIR, "index.html");
+    console.log(`Writing HTML to: ${filepath}`);
+    const template = indexTemplate(results);
+    await writeFile(filepath, await template, { flag: "w+" });
 }
+
+main().catch(console.error);
